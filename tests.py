470e21051963dd598587cbbeeaac2ec02828529e
MikhailDolgopolov <mishad2304@gmail.com>
2024-08-04 19:07:59 +0300

    updates

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from pprint import pprint
 
 from Day import Day
 from Forecast import Forecast
@@ -6,12 +7,19 @@ from ForecastRendering import render_forecast_data
 from Parsers.ForecaParser import ForecaParser
 from Parsers.GismeteoParser import GismeteoParser
 from Parsers.OpenmeteoParser import OpenmeteoParser
+from Parsers.AccuweatherParser import AccuweatherParser
 
-forecast = Forecast(rainfall_sources=["Gismeteo"])
+# forecast = Forecast(rainfall_sources=["Gismeteo"])
+#
+# d = datetime.today()
+#
+# render_forecast_data(forecast, d+timedelta(days=2), save=False, show=True)
 
-d = datetime.today()
 
-render_forecast_data(forecast, d+timedelta(days=2), save=False, show=True)
+p = AccuweatherParser()
 
+print(p.find_location_key())
 
+data = p.load_weather()
 
+# pprint(data)
